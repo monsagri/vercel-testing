6e10e2093f1177faf73c2c826e2d716cf70548e3
Fabian Feldberg <ffeldberg@launchdarkly.com>
2023-05-02 14:34:31 +0100

    new versions

--- sdk/messages.js
+++
@@ -1,5 +0,0 @@
-exports.missingKey = () => 'You must configure the client with a client key';
-
-exports.missingNamespace = () => 'You must configure the client with a Edge Config namespace binding';
-
-exports.unsupportedOption = key => `Configuration option: ${key} not supported`;
